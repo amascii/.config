d0901aa3b6e07437a55672982b90560a699a10ac
amascii <armando.s.c@protonmail.com>
2022-08-15 21:19:53 -0500

    python indent settings

--- nvim/lua/amasc/set.lua
+++ nvim/lua/amasc/set.lua
@@ -7,6 +7,9 @@ vim.opt.tabstop = 4
 -- vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.g.pyindent_open_paren = "shiftwidth()"
+vim.g.pyindent_nested_paren = "shiftwidth()"
+vim.g.pyindent_continue = "shiftwidth()"
 
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
