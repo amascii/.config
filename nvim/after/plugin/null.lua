7480f72679a37300d272b05e58b4741d03f31085
amascii <armando.s.c@protonmail.com>
2022-12-13 20:14:14 -0600

    remove null, use native lsp, use cmp

--- nvim/after/plugin/null.lua
+++
@@ -1,8 +0,0 @@
-local null_ls = require("null-ls")
-null_ls.setup({
-    sources={
-        null_ls.builtins.diagnostics.flake8,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.golangci_lint,
-    }
-})
--- nvim/lua/amasc/packer.lua
+++ nvim/lua/amasc/packer.lua
@@ -3,9 +3,15 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use "jose-elias-alvarez/null-ls.nvim"
-  use "tpope/vim-surround"
-  use "ellisonleao/gruvbox.nvim"
-  use "nvim-lua/plenary.nvim"
+    use 'wbthomason/packer.nvim'
+    use "tpope/vim-surround"
+    use "ellisonleao/gruvbox.nvim"
+    use "nvim-lua/plenary.nvim"
+    use "neovim/nvim-lspconfig"
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-buffer"
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-nvim-lua"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "saadparwaiz1/cmp_luasnip"
 end)
